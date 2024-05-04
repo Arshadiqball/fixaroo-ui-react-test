5f7ab02f1d1a1e9373eaa4ebadd22d8f5b929fe1
DESKTOP-PR3ASUM\Arshad <arshadiqbal.d@gmail.com>
2024-05-04 17:37:17 +0500

    Initialize project using Create React App

---
+++ Header.js
@@ -0,0 +1,12 @@
+// src/components/Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-xl fixed-top navbar-light custom-nav">
+      {/* Header content goes here */}
+    </nav>
+  );
+}
+
+export default Header;
